26b79e65733cca3cc2b9107195a90e9df2a2cf82
Arthur Comet <arthurcom@outlook.fr>
2025-09-22 13:51:05 +0200

    add : selection of start or exit with arrows

--- client/include/Client/Menu.hpp
+++ client/include/Client/Menu.hpp
@@ -2,7 +2,7 @@
 
 #include "Engine/Engine.hpp"
 #include "Interfaces/IRenderer.hpp"
-#include <vector>   // ⚡ nécessaire
+#include <vector>
 
 namespace cli
 {
@@ -14,17 +14,22 @@ namespace cli
 
         void handleEvent(const eng::Event &event);
         void update(float dt);
-        void unload(); // détruit les entités du menu
-
+        void unload();
         bool shouldStartGame() const { return m_startGame; }
         bool shouldExitGame() const { return m_exitGame; }
 
     private:
+        struct MenuItem {
+            ecs::Entity entity;
+            std::string id;
+        };
         eng::Engine &m_engine;
         bool m_startGame = false;
         bool m_exitGame = false;
+        int m_selectedIndex = 0;
+        std::vector<MenuItem> m_items;
 
-        std::vector<ecs::Entity> m_entities; // ⚡ liste des entités du menu
         void createMenuEntities();
+        void updateHighlight();
     };
 }
