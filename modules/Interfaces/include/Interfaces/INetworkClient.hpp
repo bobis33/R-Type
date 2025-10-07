74c339d5b4e60e5c4ea76a1b940a30c778fbd5a4
Momo <moana.piquet@epitech.eu>
2025-10-07 12:02:30 +0200

    Feat: enhance INetworkClient and INetworkServer with connection and handler management methods

--- modules/Interfaces/include/Interfaces/INetworkClient.hpp
+++ modules/Interfaces/include/Interfaces/INetworkClient.hpp
@@ -6,6 +6,12 @@
 
 #pragma once
 
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "Interfaces/Protocol/Protocol.hpp"
 #include "Utils/Interfaces/IPlugin.hpp"
 
 namespace eng
@@ -19,8 +25,33 @@ namespace eng
     class INetworkClient : public utl::IPlugin
     {
         public:
-            virtual void connect(const std::string &host, unsigned short int port) = 0;
+            using PacketHandler = std::function<void(const rnp::PacketHeader &, const std::vector<uint8_t> &)>;
+
+            virtual ~INetworkClient() = default;
+
+            // Connection management
+            virtual void connect(const std::string &host, uint16_t port) = 0;
             virtual void disconnect() = 0;
+
+            // Protocol messages
+            virtual void sendConnect(const std::string &playerName) = 0;
+            virtual void sendConnectWithCaps(const std::string &playerName, std::uint32_t clientCaps) = 0;
+            virtual void sendDisconnect() = 0;
+            virtual void sendDisconnect(rnp::DisconnectReason reason) = 0;
+            virtual void sendPlayerInput(uint8_t direction, uint8_t shooting) = 0;
+            virtual void sendPlayerInputAsEvent(std::uint16_t playerId, uint8_t direction, uint8_t shooting,
+                                               uint32_t clientTimeMs) = 0;
+            virtual void sendPing() = 0;
+            virtual void sendPing(std::uint32_t nonce, std::uint32_t sendTimeMs) = 0;
+            virtual void sendAck(std::uint32_t cumulative, std::uint32_t ackBits) = 0;
+
+            // Handler management
+            virtual void setPacketHandler(rnp::PacketType type, PacketHandler handler) = 0;
+            virtual void setEventsHandler(std::function<void(const std::vector<rnp::EventRecord> &)> handler) = 0;
+
+            // Getters
+            virtual std::uint32_t getSessionId() const = 0;
+            virtual std::uint16_t getServerTickRate() const = 0;
 
         private:
     }; // class INetworkClient
--- modules/Interfaces/include/Interfaces/INetworkServer.hpp
+++ modules/Interfaces/include/Interfaces/INetworkServer.hpp
@@ -6,6 +6,12 @@
 
 #pragma once
 
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "Interfaces/Protocol/Protocol.hpp"
 #include "Utils/Interfaces/IPlugin.hpp"
 
 namespace srv
@@ -23,9 +29,17 @@ namespace srv
     class INetworkServer : public utl::IPlugin
     {
         public:
-            virtual void init(uint16_t port, const std::string &address) = 0;
+            virtual ~INetworkServer() = default;
+
+            // Server lifecycle
             virtual void start() = 0;
             virtual void stop() = 0;
+
+            // Configuration
+            virtual void setTickRate(std::uint16_t tickRate) = 0;
+            virtual void setServerCapabilities(std::uint32_t caps) = 0;
+
+        private:
     }; // class INetworkServer
 
 } // namespace srv
